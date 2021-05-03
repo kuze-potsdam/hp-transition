c10e533049c08a15c96d8566840fb71591793151
Matthias Wernicke <matthias.wernicke@googlemail.com>
2021-05-03 23:37:18 +0200

    Initial commit

---
+++ cms/php/loginhistory.php
@@ -0,0 +1,1 @@
+<?php $failcounter=0; $failtime=0; $totalfails=0; ?>
---
+++ cms/php/savehtml.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+date_default_timezone_set('Europe/Berlin');
+if ($_SESSION["authorization"] == 'OK') {
+
+$currentfile = $_POST["currentfile"];
+if(!$currentfile){exit();}
+
+include '../settings.php';
+if ($demomode == 'on'){exit();}
+
+// CREATE A BACKUP
+$timestamp = date('Y-m-d-H-i-s-').substr((string)microtime(), 2, 2);
+$backupfile = '../../backup/'.$timestamp.'-'.$currentfile;
+copy('../../'.$currentfile, $backupfile); 
+
+if (get_magic_quotes_gpc()) {
+
+$content = stripslashes($_POST["content"]);
+
+} else {
+
+$content = $_POST["content"];
+
+}
+
+$currentfile = '../../'.$currentfile;
+
+$targetfile = fopen($currentfile,"w");
+rewind($targetfile);
+fwrite($targetfile, $content);
+fclose($targetfile);
+
+echo 'OK';
+
+}
+
+?>
